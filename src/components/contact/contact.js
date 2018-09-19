27d004a510d1b644e4637ea697bd0c78bc24ab82
Mike <wrzosekm71@gmail.com>
2018-09-19 07:46:05 +0100

    deploying

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -2,10 +2,13 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <legend style={{paddingTop: "50px"}}
-                        className="mt-2 f1 fw6 ph0 mh0">
-                        Skontaktuj się z nami
-        </legend>
+        <footer>
+            <legend style={{paddingTop: "50px"}}
+                            className="mt-2 f1 fw6 ph0 mh0">
+                            Skontaktuj się z nami
+            </legend>
+        </footer>
+        
     );
 }
 
